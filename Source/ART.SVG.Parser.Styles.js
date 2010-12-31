5ec4a58903a8d0b2ab0f58b770079631cf75f65c
Sebastian Markbage <sebastian@calyptus.eu>
2010-12-31 07:05:38 +0100

    Only on own properties though...

--- Source/ART.SVG.Parser.Styles.js
+++ Source/ART.SVG.Parser.Styles.js
@@ -14,7 +14,9 @@ ART.SVG.Parser.implement({
 			css = this.css;
 		}
 		css.applyStyle(element, target);
-		for (var key in target) if (target[key] == 'inherit') delete target[key];
+		for (var key in target)
+			if (target.hasOwnProperty(key) && target[key] == 'inherit')
+				delete target[key];
 		applyStyles.call(this, element, target);
 	}
 
