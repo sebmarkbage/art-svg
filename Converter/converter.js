5f892637865e82be8ca7e3b2b77057696e7a1e99
Sebastian Markbage <sebastian@calyptus.eu>
2011-03-27 22:18:26 +0200

    Added SVG to ART Script Converter Demo

---
+++ Converter/converter.js
@@ -0,0 +1,15 @@
+new Request.XML({
+
+	url: location.search.substr(1) || 'testcase.svg',
+
+	onSuccess:  function(txt, doc){
+	
+		var klass = ART.SVG.parse(doc).toClass();
+	
+		var result = document.createElement('textarea');
+		result.value = 'var Sample = ' + klass + ';';
+		
+		document.body.appendChild(result);
+	}
+
+}).get();
